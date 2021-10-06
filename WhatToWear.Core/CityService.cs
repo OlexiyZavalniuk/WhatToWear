e6ba1b0fe41f22e7c29aaea8ab57820e4e4c2ac8
Olexiy Zavalniuk <83496121+OlexiyZavalniuk@users.noreply.github.com>
2021-10-06 17:08:30 +0300

    Create first simple api for geting city by name

---
+++ WhatToWear.Core/CityService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using WhatToWear.Models.Models;
+using WhatToWear.Database;
+using System.Linq;
+
+namespace WhatToWear.Core
+{
+    public static class CityService
+    {
+        public static IEnumerable<City> GetCities(string toFind)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                return db.Cities.Where(c => c.Name == toFind).ToList();
+            }
+        }
+    }
+}
---
+++ WhatToWear/Controllers/CitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatToWear.Models.Models;
+using WhatToWear.Core;
+
+namespace WhatToWear.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class citiesController : ControllerBase
+    {
+
+        private readonly ILogger<citiesController> _logger;
+
+        public citiesController(ILogger<citiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("{name}")]
+        [HttpGet]
+        public IEnumerable<City> Get(string name)
+        {
+            return CityService.GetCities(name);
+        }
+    }
+}
